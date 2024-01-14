073fc23a6381b14b6fafdb9f605337abe48b176b
prajwal2004-07 <pbawanthare0007@gmail.com>
2024-01-14 16:23:06 +0530

    Day1 of Learning Javascript

---
+++ Day1.js
@@ -0,0 +1,39 @@
+//Introduction to Javascript
+
+// Day 1 of  Learning Javascript
+
+// Variable
+
+// Variable Define : Variable is a Container That Conatin the Different Datatype Values
+
+console.log ("Variables : let , var & const");
+
+// Three Types of Variable 
+//var, lrt and const
+
+var a = 1
+console.log(a);
+var a = "Kite";
+console.log(a);
+
+// var Variable Can be Redeclared and Reassigned
+
+let b = 2;
+console.log(b);
+//let b = "Manja"; It shows Error
+b = "Manja";
+console.log(b);
+
+// let variable can not be redeclared but it can be reassigned
+
+const c = 3;
+console.log(c);
+// c = "kite manja";  It shows error
+
+//const variable can not be redeclared or reassigned
+
+
+// Primitives Datatypes
+
+
+
